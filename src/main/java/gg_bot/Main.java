3e9cfea4116e7476d43ae108fae9a20290515c5b
Revc0 <143342816+Revc0@users.noreply.github.com>
2023-09-27 21:00:51 +0200

    Added Files
    Java Discord Bot made with Discord4J. Currently responds to any message with GG!. So yeah GG!

---
+++ src/main/java/gg_bot/Main.java
@@ -0,0 +1,40 @@
+package gg_bot;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
+
+public class Main {
+    public static void main(String[] args) {
+        String token = System.getenv("DISCORD_BOT_TOKEN");
+
+        DiscordClient client = DiscordClient.create(token);
+        client.withGateway((GatewayDiscordClient gateway) -> {
+
+            Mono<Void> printOnLogin = gateway.on(ReadyEvent.class, event ->
+                            Mono.fromRunnable(() -> {
+                                final User self = event.getSelf();
+                                System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+                            }))
+                    .then();
+
+            Mono<Void> handleMessage = gateway.on(MessageCreateEvent.class, event -> {
+                Message message = event.getMessage();
+                // You may want to ensure the message is not from the bot itself
+                if (!message.getAuthor().map(user -> user.getId().equals(gateway.getSelfId())).orElse(true)) {
+
+                    return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("GG!"))
+                            .then();
+                }
+                return Mono.empty();
+            }).then();
+
+            return Mono.when(printOnLogin, handleMessage);
+        }).block();
+    }
+}
